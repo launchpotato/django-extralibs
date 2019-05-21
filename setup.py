69955732df4ca0e8e25d7b82171a6402010ca8fd
Gilang Chandrasa <gilang.chandrasa@gmail.com>
2019-05-21 15:40:55 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ except ImportError:
 
 setup(
     name='django-extralibs',
-    version='0.1.4',
+    version='0.1.6',
     description='A collection of extra libraries for django',
     author='Gilang Chandrasa',
     author_email='gilang@launchpotato.com',
     url='https://github.com/launchpotato/django-extralibs',
     packages=[
         'django_extralibs',
+        'django_extralibs/templatetags',
     ],
 )
